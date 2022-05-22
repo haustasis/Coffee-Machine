16178f6b73d7cbf4a1c7cba3f05475245b4a68c5
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-05-22 10:08:35 +0200

    Stage 2/6: Ingredient calculator

--- machine/CoffeeMachine.java
+++ machine/CoffeeMachine.java
@@ -1,14 +1,32 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("""
-                Starting to make a coffee
-                Grinding coffee beans
-                Boiling water
-                Mixing boiled water with crushed coffee beans
-                Pouring coffee into the cup
-                Pouring some milk into the cup
-                Coffee is ready!""");
+
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Pouring some milk into the cup");
+        System.out.println("Coffee is ready!");
+
+        Scanner sc = new Scanner(System.in);
+
+        int cups = sc.nextInt();
+
+        int water = cups * 200;
+        int milk = cups * 50;
+        int beans = cups * 15;
+
+        System.out.printf("For %d cups of coffee you will need: %n", cups);
+        System.out.printf("%d ml of water %n", water);
+        System.out.printf("%d ml of milk %n", milk);
+        System.out.printf("%d g of coffee beans %n", beans);
+
+
+
     }
 }
